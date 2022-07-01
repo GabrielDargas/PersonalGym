d558f9e5c1e6f7b470cee2682e1bc7dd24c85b55
GabrielDargas <gabriel_dargas2@hotmail.com>
2022-07-01 00:05:08 -0300

    Tradução/Inglês

--- app/src/main/java/br/com/gabrieldargas/personalgym/ui/exercicio/ExercicioFragment.kt
+++ app/src/main/java/br/com/gabrieldargas/personalgym/ui/exercicio/ExercicioFragment.kt
@@ -12,6 +12,8 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.LiveData
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import br.com.gabrieldargas.personalgym.R
 import br.com.gabrieldargas.personalgym.databinding.FragmentExercicioBinding
 import br.com.gabrieldargas.personalgym.databinding.FragmentSignupBinding
@@ -19,15 +21,33 @@ import br.com.gabrieldargas.personalgym.models.Exercicio
 import br.com.gabrieldargas.personalgym.ui.base.BaseFragment
 import br.com.gabrieldargas.personalgym.ui.base.auth.BaseAuthFragment
 import br.com.gabrieldargas.personalgym.ui.login.LoginViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class ExercicioFragment : BaseAuthFragment() {
 
+    private var mAuth: FirebaseAuth = FirebaseAuth.getInstance()
     override val layout = R.layout.fragment_exercicio
     private lateinit var btcadastrarNovoExercicio: Button
+    private val db = FirebaseFirestore.getInstance()
+    private lateinit var recyclerView : RecyclerView
+    private var exercicios = emptyList<Exercicio>()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view: View = inflater.inflate(R.layout.fragment_exercicio, container, false)
+        recyclerView = view.findViewById(R.id.rv_firedb)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.setLayoutManager(LinearLayoutManager(view.getContext()))
+        recyclerView.setAdapter(ExercicioListAdapter)
+        return view
+    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setUpView(view)
 
@@ -37,7 +57,8 @@ class ExercicioFragment : BaseAuthFragment() {
         btcadastrarNovoExercicio = view.findViewById(R.id.btcadastrarNovoExercicio)
         btcadastrarNovoExercicio.setOnClickListener{
             findNavController().navigate(R.id.cadastroFragment)
-        }
+        }*/
+
     }
 
 
---
+++ app/src/main/java/br/com/gabrieldargas/personalgym/ui/exercicio/ExercicioViewHolder.kt
@@ -0,0 +1,15 @@
+package br.com.gabrieldargas.personalgym.ui.exercicio
+
+import android.R
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class ExercicioViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    val view: TextView
+
+    init {
+        view = itemView.findViewById(R.id.)
+    }
+}
